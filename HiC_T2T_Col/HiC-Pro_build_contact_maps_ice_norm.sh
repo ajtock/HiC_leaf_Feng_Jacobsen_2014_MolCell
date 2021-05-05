7c0738f72638ff2a78d2b1f758b51eef0bec0f21
ajtock <andytock@gmail.com>
2021-05-05 15:02:01 +0100

    HiC-Pro scripts

--- HiC_T2T_Col/HiC-Pro_build_contact_maps_ice_norm.sh
+++ HiC_T2T_Col/HiC-Pro_build_contact_maps_ice_norm.sh
@@ -2,7 +2,7 @@
 
 # HiC-Pro version 3.0.0
 
-# Run HiC-Pro on gzip-compressed FASTQ files located within subdirectories of raw_data/
+# Run HiC-Pro steps "build_contact_maps" and "ice_norm"
 
 # Note that the following scripts in the /home/ajt200/envs_miniconda3/HiC_Pro/HiC-Pro_3.0.0/annotation/ directory must be run before running HiC-Pro.sh,
 # and the output files containing chromosome sizes and restriction site fragments must be located in the working directory
@@ -16,6 +16,8 @@ source activate HiC_Pro
 
 ${EXECDIR}/HiC-Pro --input rawdata/ \
                    --output results/ \
-                   --conf config-hicpro.txt
+                   --conf config-hicpro.txt \
+                   --step build_contact_maps \
+                   --step ice_norm
 
 conda deactivate
