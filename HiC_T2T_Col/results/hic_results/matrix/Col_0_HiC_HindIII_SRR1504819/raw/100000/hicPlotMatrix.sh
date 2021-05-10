87c8332c2adebb1fe32f8c7647c084b8aa185107
ajtock <andytock@gmail.com>
2021-05-10 16:10:38 +0100

    HiCExplorer scripts

---
+++ HiC_T2T_Col/results/hic_results/matrix/Col_0_HiC_HindIII_SRR1504819/raw/100000/hicPlotMatrix.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# hicPlotMatrix from HiCExplorer version 3.6
+
+# From https://hicexplorer.readthedocs.io/en/latest/content/tools/hicPlotMatrix.html
+
+# Usage:
+# ./hicPlotMatrix.sh Col_0_HiC_HindIII_SRR1504819 T2T_Col '100000' norm_range KR
+
+SAMPLE=$1
+GENOME=$2
+RESOLUTION=$3
+NORM=$4
+CORRECTION=$5
+
+[ -d plots/ ] || mkdir -p plots/
+
+source activate HiCExplorer
+
+hicPlotMatrix --matrix ${SAMPLE}_${GENOME}_${RESOLUTION}_${NORM}_${CORRECTION}.h5 \
+              --outFileName plots/${SAMPLE}_${GENOME}_${RESOLUTION}_${NORM}_${CORRECTION}.pdf \
+              --perChromosome \
+              --chromosomeOrder Chr1 Chr2 Chr3 Chr4 Chr5 \
+              --scoreName "Hi-C ${CORRECTION} counts" \
+              --vMin 0.0 \
+              --vMax 0.9 \
+              --rotationX 45 \
+              --dpi 300 \
+              --increaseFigureWidth 1 \
+              --increaseFigureHeight 0.1 \
+              --title ${SAMPLE}_${GENOME}_${RESOLUTION}_${NORM}_${CORRECTION}
+
+conda deactivate
