7fb2fedb30941a52bd1d030802b07f5d632b8ca3
ajtock <andytock@gmail.com>
2021-05-18 13:22:43 +0100

    HiCExplorer scripts

--- HiC_T2T_Col/results/hic_results/matrix/Col_0_HiC_HindIII_SRR1504819/raw/100000/hicFindTADs.sh
+++ HiC_T2T_Col/results/hic_results/matrix/Col_0_HiC_HindIII_SRR1504819/raw/100000/hicFindTADs.sh
@@ -5,7 +5,7 @@
 # From https://hicexplorer.readthedocs.io/en/latest/content/tools/hicFindTADs.html
 
 # Usage:
-# ./hicFindTADs.sh Col_0_HiC_HindIII_SRR1504819 T2T_Col '20000' norm_range KR 0.05
+# csmit -m 20G -c 47 "bash ./hicFindTADs.sh log2ratio_met1_3_HiC_HindIII_SRR1504825_Col_0_HiC_HindIII_SRR1504819 T2T_Col '100000' norm_range KR 0.05"
 
 SAMPLE1=$1
 GENOME=$2
@@ -21,8 +21,8 @@ hicFindTADs --matrix ${SAMPLE1}_${GENOME}_${RESOLUTION}_${NORM}_${CORRECTION}.h5
             --correctForMultipleTesting fdr \
             --thresholdComparisons ${THRESHOLD} \
             --delta 0.01 \
-            --minDepth 60000 \
-            --maxDepth 120000 \
+            --minDepth $(expr $RESOLUTION \* 3) \
+            --maxDepth $(expr $RESOLUTION \* 10) \
             --step ${RESOLUTION} \
             --chromosomes Chr1 Chr2 Chr3 Chr4 Chr5 \
             --numberOfProcessors 48
